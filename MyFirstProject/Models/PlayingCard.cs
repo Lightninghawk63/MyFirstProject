fa6beb496794aef443d452fba3a3f8cf63f9f66c
Lightninghaw63 <joshdleroy@gmail.com>
2021-12-08 15:06:32 -0600

    ListViewButtons

--- MyFirstProject/Models/PlayingCard.cs
+++ MyFirstProject/Models/PlayingCard.cs
@@ -10,6 +10,7 @@ namespace MyFirstProject.Models
         public string Name { get; set; }
         public string Suit { get; set; }
         public string Image { get; set; }
+       public int Index { get; set; }
 
         public PlayingCard(string n, string s, string i)
         {
@@ -17,7 +18,14 @@ namespace MyFirstProject.Models
             Suit = s;
             Image = i;
         }
-        
+
+        public PlayingCard(string n, string s, int i)
+        {
+            Name = n;
+            Suit = s;
+            Index = i;
+        }
+
         public static List<PlayingCard> GetCard()
         {
             return new List<PlayingCard>
@@ -26,6 +34,17 @@ namespace MyFirstProject.Models
                 new PlayingCard("Queen", "Diamonds", "https://www.aquariusmaximus.com/wp-content/uploads/2016/07/queendiamonds.png"),
                 new PlayingCard("King", "Hearts", "https://www.aquariusmaximus.com/wp-content/uploads/2016/07/kinghearts.png")
             };
-        }  
+        }
+
+        public static List<PlayingCard> GetCardButton()
+        {
+            return new List<PlayingCard>
+            {
+                new PlayingCard("Jack", "Clubs", 0),
+                new PlayingCard("Queen", "Diamonds", 1),
+                new PlayingCard("King", "Hearts", 2),
+                new PlayingCard("Ace", "Spades", 3)
+            };
+        }
     }
 }
--- MyFirstProject/ViewViewModel/ListView/ListViewButtons/ListViewButtonsViewModel.cs
+++ MyFirstProject/ViewViewModel/ListView/ListViewButtons/ListViewButtonsViewModel.cs
@@ -1,18 +1,68 @@
 ﻿using MyFirstProject.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace MyFirstProject.ViewViewModel.ListView.ListViewButtons
 {
     public class ListViewButtonsViewModel : ContentPage
     {
+        private List<PlayingCard> _card;
+        private ObservableCollection<PlayingCard> _cards;
+
         public ListViewButtonsViewModel()
         {
             Title = Titles.ListViewButtonsTitle;
+
+            _cards = new ObservableCollection<PlayingCard>();
+            _card = PlayingCard.GetCardButton();
+            this.LoadCards();
         }
+
+        private void LoadCards()
+        {
+            try
+            {
+                _cards.Clear();
+                foreach (var c in _card)
+                {
+                    Cards.Add(c);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+
+        public ObservableCollection<PlayingCard> Cards
+        {
+            get
+            {
+                return _cards;
+            }
+            set
+            {
+                _cards = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command<PlayingCard> DeleteCommand
+        {
+            get
+            {
+                return new Command<PlayingCard>((PlayingCard mov) =>
+                {
+                    Cards.Remove(mov);
+                });
+            }
+        }
+    
     }
 }
