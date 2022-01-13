c674155633ccc94ff90d7e1a4928d6932d8141c7
Lightninghaw63 <joshdleroy@gmail.com>
2022-01-13 10:40:16 -0600

    slider

--- MyFirstProject/ViewViewModel/Controls/ControlsMenuViewModel.cs
+++ MyFirstProject/ViewViewModel/Controls/ControlsMenuViewModel.cs
@@ -1,16 +1,26 @@
 ﻿using MyFirstProject.Models;
+using MyFirstProject.ViewViewModel.Controls.Slider;
 using MyFirstProject.ViewViewModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace MyFirstProject.ViewViewModel.Controls
 {
     public class ControlsMenuViewModel : BaseViewModel
     {
+        public ICommand OnSliderClicked { get; set; }
         public ControlsMenuViewModel()
         {
             Title = Titles.ControlsTitle;
+            OnSliderClicked = new Command(OnSliderClickedAsync);
+        }
+
+        private async void OnSliderClickedAsync(object obj)
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new SliderView());
         }
     }
 }
